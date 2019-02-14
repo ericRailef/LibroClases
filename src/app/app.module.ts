eafdf1697987c7f93e8d1d1527c5d02030c4a61e
earailef <earailef@vectoritcgroup.com>
2019-02-14 17:31:20 -0300

    creacion de componente asistencia y modelos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { APP_ROUTING } from './app.routes';
+import { AsistenciaComponent } from './components/asistencia/asistencia.component';
+
+// Archivo de Rutas de la aplicación
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    AsistenciaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
